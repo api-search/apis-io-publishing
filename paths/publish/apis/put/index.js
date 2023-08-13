4e5e7e495fc0d17dc6c6fbb27b43e3d3778491c4
Kin Lane <kinlane@gmail.com>
2023-08-13 18:16:30 -0400

    working!

--- paths/publish/apis/put/index.js
+++ paths/publish/apis/put/index.js
@@ -115,14 +115,14 @@ exports.handler = vandium.generic()
                   //var body = Buffer.concat(body).toString();
                   //console.log(body);
                   var github_results = JSON.parse(body);
-                  console.log(github_results);
+                  //console.log(github_results);
 
                   var sha = '';
                   if(github_results.sha){
                     sha = github_results.sha;
                   }
 
-                  console.log(sha);
+                  //console.log(sha);
                   var api_yaml = yaml.dump(publish_api);
                   api_yaml = '---\r\n' + api_yaml + '---\r\n';
 
