be54761e0c12d1932e9a63212cf549877bdf49e1
Kin Lane <kinlane@gmail.com>
2023-08-13 09:33:55 -0400

    update

--- paths/publish/apis/put/index.js
+++ paths/publish/apis/put/index.js
@@ -10,25 +10,68 @@ exports.handler = vandium.generic()
     password : process.env.password,
     database : process.env.database
     });
-    
-    var total_properties = Object.keys(event.body).length;
 
-    var sql = 'UPDATE apis SET ';
+    let currentDate = new Date();
+    let startDate = new Date(currentDate.getFullYear(), 0, 1);
+    let days = Math.floor((currentDate - startDate) / (24 * 60 * 60 * 1000));
+     
+    const weekNumber = Math.ceil(days / 7);     
     
-    var property_count = 1;
-    for (const [key, value] of Object.entries(event.body)) {
-      sql += key + " = '" + value + "'";
-      if(property_count != total_properties){
-        sql += ',';
-      }
-      property_count++;
-    }
-
-    sql += " WHERE id = " + connection.escape(event.api_id);
-  
+    var sql = 'SELECT * FROM apis WHERE published <> ' + weekNumber;
     connection.query(sql, function (error, results, fields) {
 
-    callback( null );
+      if(results && results.length > 0){
+        
+        // Pull any new ones.
+        var apisjson_name = results[0].name;
+        var apisjson_url = results[0].apisjson_url;
+        var apisjson_slug = apisjson_url.replace('http://','http-');
+        apisjson_slug = apisjson_slug.replace('.json','');
+        apisjson_slug = apisjson_slug.replace('.yaml','');
+        apisjson_slug = apisjson_slug.replace('https://','https-');
+        apisjson_slug = apisjson_slug.replace(/\//g, '-');
+        apisjson_slug = apisjson_slug.replace('.','-');
+
+        var local_apis_json = "https://kinlane-productions2.s3.amazonaws.com/" + results[0].path;
+        console.log(local_apis_json);
+
+        https.get(local_apis_json, res => {
+          
+          let data = [];
+          //const headerDate = res.headers && res.headers.date ? res.headers.date : 'no response date';
+          
+          //console.log('Status Code:', res.statusCode);
+          //console.log('Date in Response header:', headerDate);
+        
+          res.on('data', chunk => {
+            data.push(chunk);
+          });
+        
+          res.on('end', () => {
+
+            var apisjson = JSON.stringify(Buffer.concat(data).toString());
+
+            callback( null, apisjson );
+
+
+          });
+        }).on('error', err => {
+          var response = {};
+          response['pulling'] = "Problem pulling the APIs.json.";            
+          callback( null, response );  
+        });        
+  
+      }
+      else{
+        
+        // Pull one that is old
+        var response = {};
+        response['pulling'] = "No more to rate.";            
+        callback( null, response );  
+        connection.end();        
+          
+      }      
 
   });
+
 });
