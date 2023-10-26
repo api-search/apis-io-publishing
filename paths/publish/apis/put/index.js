6dc9f53142edad10df59a1c995b94f1e69f8e193
Kin Lane <kinlane@gmail.com>
2023-10-26 19:08:51 -0400

    update

--- paths/publish/apis/put/index.js
+++ paths/publish/apis/put/index.js
@@ -26,77 +26,10 @@ exports.handler = vandium.generic()
     var sql = 'select a.name,ao.name as name2,a.slug,ao.slug as slug2,a.description,ao.description AS description2,a.image,ao.image AS image2,a.baseURL,a.humanURL,a.apisjson_url,a.tags,a.published,(select name from apisjson_overlay aj WHERE aj.apisjson_url = a.apisjson_url) as indexName,(select score from apisjson aj WHERE aj.url = a.apisjson_url) as score,(select percentage from apisjson aj WHERE aj.url = a.apisjson_url) as percentage,(select rules from apisjson aj WHERE aj.url = a.apisjson_url) as rules FROM apis a LEFT JOIN apis_overlay ao ON a.humanURL = ao.humanURL WHERE a.published <> ' + weekNumber;
     connection.query(sql, function (error, results, fields) {
 
-      if(results && results.length > 0){
-
-        var apis_name = results[0].name2;
-        var apis_slug = results[0].slug2;
-        var apis_description = results[0].description;
-        var apis_image = results[0].image;
-        var apis_tags = results[0].tags;
-        var apis_human_url = results[0].humanUrl;
-        var apis_base_url = results[0].baseURL;
-        var apis_score = results[0].score;
-        var apis_percentage = results[0].percentage;
-        var apis_rules = results[0].rules;
-        var apisjson_url = results[0].apisjson_url;
-        
-        var publish_api = {};
-        
-        // Needed for Static Layout
-        publish_api.layout = "post";
-        publish_api.published = true;
-
-        // Main API Details
-        publish_api.name = apis_name;
-        publish_api.description = apis_description;
-        publish_api.image = apis_image;
-        publish_api.tags = apis_tags.split(',');
-      
-        publish_api.humanURL = apis_human_url;
-        publish_api.baseURL = apis_base_url;
-
-        publish_api.score = apis_score;
-        publish_api.percentage = apis_percentage;
-        publish_api.rules = apis_rules;        
-
-        var sql2 = 'select p.type,p.url FROM properties p WHERE p.api_base_url = ' + apis_base_url + ' AND common = 0';
-        connection.query(sql2, function (error2, results2, fields2) {
-    
-          if(results2 && results2.length > 0){      
-                        
-            publish_api.properties = results2;
-
-            var sql3 = 'select p.type,p.url FROM properties p WHERE p.api_base_url = ' + apisjson_url + ' AND common = 1';
-            connection.query(sql3, function (error3, results3, fields3) {
-        
-              if(results3 && results3.length > 0){      
-                            
-                publish_api.common = results3;            
-
-                var response = {};
-                response['pulling'] = "blah";            
-                callback( null, results3 );  
-                connection.end();                  
-                
-
-              }
-
-            });  // End Common                
-          
-          }
-
-        });  // End Properties                         
-  
-      }
-      else{
-        
-        // Pull one that is old
-        var response = {};
-        response['pulling'] = "No more to publish.";            
-        callback( null, response );  
-        connection.end();        
-          
-      }      
+      var response = {};
+      response['pulling'] = "Wa Wah";            
+      callback( null, results );  
+      connection.end();              
 
   });
 
